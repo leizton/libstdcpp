29d23afb4db9203726e954104149db9d6262aaa4
leizton <leizton@126.com>
2020-09-24 20:50:11 +0800

    fmt

--- include/bits/stl_iterator_base_types.h
+++ include/bits/stl_iterator_base_types.h
@@ -64,18 +64,17 @@
 #include <bits/c++config.h>
 
 #if __cplusplus >= 201103L
-# include <type_traits>  // For __void_t, is_convertible
+#include <type_traits> // For __void_t, is_convertible
 #endif
 
-namespace std _GLIBCXX_VISIBILITY(default)
-{
-_GLIBCXX_BEGIN_NAMESPACE_VERSION
+namespace std _GLIBCXX_VISIBILITY(default) {
+  _GLIBCXX_BEGIN_NAMESPACE_VERSION
 
   /**
    *  @defgroup iterators Iterators
    *  Abstractions for uniform iterating through various underlying types.
   */
-  //@{ 
+  //@{
 
   /**
    *  @defgroup iterator_tags Iterator Tags
@@ -84,23 +83,23 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
    *  are.  Different underlying algorithms can then be used based on the
    *  different operations supported by different iterator types.
   */
-  //@{ 
+  //@{
   ///  Marking input iterators.
-  struct input_iterator_tag { };
+  struct input_iterator_tag {};
 
   ///  Marking output iterators.
-  struct output_iterator_tag { };
+  struct output_iterator_tag {};
 
   /// Forward iterators support a superset of input iterator operations.
-  struct forward_iterator_tag : public input_iterator_tag { };
+  struct forward_iterator_tag : public input_iterator_tag {};
 
   /// Bidirectional iterators support a superset of forward iterator
   /// operations.
-  struct bidirectional_iterator_tag : public forward_iterator_tag { };
+  struct bidirectional_iterator_tag : public forward_iterator_tag {};
 
   /// Random-access iterators support a superset of bidirectional
   /// iterator operations.
-  struct random_access_iterator_tag : public bidirectional_iterator_tag { };
+  struct random_access_iterator_tag : public bidirectional_iterator_tag {};
   //@}
 
   /**
@@ -113,21 +112,20 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
    *  In particular, there are no default implementations of requirements
    *  such as @c operator++ and the like.  (How could there be?)
   */
-  template<typename _Category, typename _Tp, typename _Distance = ptrdiff_t,
-           typename _Pointer = _Tp*, typename _Reference = _Tp&>
-    struct iterator
-    {
-      /// One of the @link iterator_tags tag types@endlink.
-      typedef _Category  iterator_category;
-      /// The type "pointed to" by the iterator.
-      typedef _Tp        value_type;
-      /// Distance between iterators is represented as this type.
-      typedef _Distance  difference_type;
-      /// This type represents a pointer-to-value_type.
-      typedef _Pointer   pointer;
-      /// This type represents a reference-to-value_type.
-      typedef _Reference reference;
-    };
+  template <typename _Category, typename _Tp, typename _Distance = ptrdiff_t,
+            typename _Pointer = _Tp*, typename _Reference = _Tp&>
+  struct iterator {
+    /// One of the @link iterator_tags tag types@endlink.
+    typedef _Category iterator_category;
+    /// The type "pointed to" by the iterator.
+    typedef _Tp value_type;
+    /// Distance between iterators is represented as this type.
+    typedef _Distance difference_type;
+    /// This type represents a pointer-to-value_type.
+    typedef _Pointer pointer;
+    /// This type represents a reference-to-value_type.
+    typedef _Reference reference;
+  };
 
   /**
    *  @brief  Traits class for iterators.
@@ -140,103 +138,91 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 #if __cplusplus >= 201103L
   // _GLIBCXX_RESOLVE_LIB_DEFECTS
   // 2408. SFINAE-friendly common_type/iterator_traits is missing in C++14
-  template<typename _Iterator, typename = __void_t<>>
-    struct __iterator_traits { };
+  template <typename _Iterator, typename = __void_t<>>
+  struct __iterator_traits {};
 
-  template<typename _Iterator>
-    struct __iterator_traits<_Iterator,
-			     __void_t<typename _Iterator::iterator_category,
-				      typename _Iterator::value_type,
-				      typename _Iterator::difference_type,
-				      typename _Iterator::pointer,
-				      typename _Iterator::reference>>
-    {
-      typedef typename _Iterator::iterator_category iterator_category;
-      typedef typename _Iterator::value_type        value_type;
-      typedef typename _Iterator::difference_type   difference_type;
-      typedef typename _Iterator::pointer           pointer;
-      typedef typename _Iterator::reference         reference;
-    };
+  template <typename _Iterator>
+  struct __iterator_traits<_Iterator,
+                           __void_t<typename _Iterator::iterator_category,
+                                    typename _Iterator::value_type,
+                                    typename _Iterator::difference_type,
+                                    typename _Iterator::pointer,
+                                    typename _Iterator::reference>> {
+    typedef typename _Iterator::iterator_category iterator_category;
+    typedef typename _Iterator::value_type value_type;
+    typedef typename _Iterator::difference_type difference_type;
+    typedef typename _Iterator::pointer pointer;
+    typedef typename _Iterator::reference reference;
+  };
 
-  template<typename _Iterator>
-    struct iterator_traits
-    : public __iterator_traits<_Iterator> { };
+  template <typename _Iterator>
+  struct iterator_traits
+      : public __iterator_traits<_Iterator> {};
 #else
-  template<typename _Iterator>
-    struct iterator_traits
-    {
-      typedef typename _Iterator::iterator_category iterator_category;
-      typedef typename _Iterator::value_type        value_type;
-      typedef typename _Iterator::difference_type   difference_type;
-      typedef typename _Iterator::pointer           pointer;
-      typedef typename _Iterator::reference         reference;
-    };
+  template <typename _Iterator>
+  struct iterator_traits {
+    typedef typename _Iterator::iterator_category iterator_category;
+    typedef typename _Iterator::value_type value_type;
+    typedef typename _Iterator::difference_type difference_type;
+    typedef typename _Iterator::pointer pointer;
+    typedef typename _Iterator::reference reference;
+  };
 #endif
 
   /// Partial specialization for pointer types.
-  template<typename _Tp>
-    struct iterator_traits<_Tp*>
-    {
-      typedef random_access_iterator_tag iterator_category;
-      typedef _Tp                         value_type;
-      typedef ptrdiff_t                   difference_type;
-      typedef _Tp*                        pointer;
-      typedef _Tp&                        reference;
-    };
+  template <typename _Tp>
+  struct iterator_traits<_Tp*> {
+    typedef random_access_iterator_tag iterator_category;
+    typedef _Tp value_type;
+    typedef ptrdiff_t difference_type;
+    typedef _Tp* pointer;
+    typedef _Tp& reference;
+  };
 
   /// Partial specialization for const pointer types.
-  template<typename _Tp>
-    struct iterator_traits<const _Tp*>
-    {
-      typedef random_access_iterator_tag iterator_category;
-      typedef _Tp                         value_type;
-      typedef ptrdiff_t                   difference_type;
-      typedef const _Tp*                  pointer;
-      typedef const _Tp&                  reference;
-    };
+  template <typename _Tp>
+  struct iterator_traits<const _Tp*> {
+    typedef random_access_iterator_tag iterator_category;
+    typedef _Tp value_type;
+    typedef ptrdiff_t difference_type;
+    typedef const _Tp* pointer;
+    typedef const _Tp& reference;
+  };
 
   /**
    *  This function is not a part of the C++ standard but is syntactic
    *  sugar for internal library use only.
   */
-  template<typename _Iter>
-    inline constexpr
-    typename iterator_traits<_Iter>::iterator_category
-    __iterator_category(const _Iter&)
-    { return typename iterator_traits<_Iter>::iterator_category(); }
+  template <typename _Iter>
+  inline constexpr
+      typename iterator_traits<_Iter>::iterator_category
+      __iterator_category(const _Iter&) { return typename iterator_traits<_Iter>::iterator_category(); }
 
   //@}
 
 #if __cplusplus < 201103L
   // If _Iterator has a base returns it otherwise _Iterator is returned
   // untouched
-  template<typename _Iterator, bool _HasBase>
-    struct _Iter_base
-    {
-      typedef _Iterator iterator_type;
-      static iterator_type _S_base(_Iterator __it)
-      { return __it; }
-    };
+  template <typename _Iterator, bool _HasBase>
+  struct _Iter_base {
+    typedef _Iterator iterator_type;
+    static iterator_type _S_base(_Iterator __it) { return __it; }
+  };
 
-  template<typename _Iterator>
-    struct _Iter_base<_Iterator, true>
-    {
-      typedef typename _Iterator::iterator_type iterator_type;
-      static iterator_type _S_base(_Iterator __it)
-      { return __it.base(); }
-    };
+  template <typename _Iterator>
+  struct _Iter_base<_Iterator, true> {
+    typedef typename _Iterator::iterator_type iterator_type;
+    static iterator_type _S_base(_Iterator __it) { return __it.base(); }
+  };
 #endif
 
 #if __cplusplus >= 201103L
-  template<typename _InIter>
-    using _RequireInputIter = typename
-      enable_if<is_convertible<typename
-		iterator_traits<_InIter>::iterator_category,
-			       input_iterator_tag>::value>::type;
+  template <typename _InIter>
+  using _RequireInputIter = typename enable_if<is_convertible<typename iterator_traits<_InIter>::iterator_category,
+                                                              input_iterator_tag>::value>::type;
 #endif
 
-_GLIBCXX_END_NAMESPACE_VERSION
-} // namespace
+  _GLIBCXX_END_NAMESPACE_VERSION
+} // namespace )
 
 #endif /* _STL_ITERATOR_BASE_TYPES_H */
-
